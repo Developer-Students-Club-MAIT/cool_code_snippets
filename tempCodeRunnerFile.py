8885c92e89b658563ff2f22a7242468fc52d3f35
ItIsUday <57130884+ItIsUday@users.noreply.github.com>
2020-10-11 18:39:31 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def fizzbuzz():
-    for num in range(1, 101):
-        fizz = 'Fizz' if num % 3 == 0 else ''
-        buzz = 'Buzz' if num % 5 == 0 else ''
-        print(f'{fizz}{buzz}') if fizz or buzz else print(num)
-
-fizzbuzz()
